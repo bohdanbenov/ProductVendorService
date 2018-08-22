0b9c360bf385d33dbd1fa4ab04e2533f7d025446
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-22 18:04:13 +0300

    #3 Edit print_list method.

--- vendor.rb
+++ vendor.rb
@@ -1,6 +1,8 @@
 require_relative 'product.rb'
 
 class Vendor
+  attr_reader :name_of_vendor
+
   def initialize(name, product)
     @name_of_vendor = name
     @products_of_vendor = product
@@ -9,6 +11,6 @@ class Vendor
   def print_list_of_products
     puts 'List of vendor\'s product'
     puts "Name of vendor: #{@name_of_vendor}"
+    @products_of_vendor.each {|key, value| puts "\tCategory of product: #{key}, price: #{value.price_of_product}"}
   end
-
 end
