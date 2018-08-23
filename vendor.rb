a8e8beafeec680c6c512b25e5dd57c9db3639034
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-23 11:13:42 +0300

    #3 Change names of variables in file vendor.rb

--- vendor.rb
+++ vendor.rb
@@ -3,14 +3,14 @@ require_relative 'product.rb'
 class Vendor
   attr_reader :name_of_vendor
 
-  def initialize(name, product)
-    @name_of_vendor = name
-    @products_of_vendor = product
+  def initialize(name, products)
+    @name = name
+    @products = products
   end
 
   def print_list_of_products
-    puts "\nName of vendor: #{@name_of_vendor}"
+    puts "\nName of vendor: #{@name}"
     puts 'List of vendor\'s product'
-    @products_of_vendor.each {|product| puts "\tName: #{product.name_of_product}, price: #{product.price_of_product}"}
+    @products.each {|product| puts "\tName: #{product.name_of_product}, price: #{product.price_of_product}"}
   end
 end
